b1d953388b97a074b78f279b8b6f047c52862c83
Justin Hui <jhui@lcs.on.ca>
2023-04-14 14:52:09 -0400

    Made the view use the model.

--- Jokes/Views/JokeView.swift
+++ Jokes/Views/JokeView.swift
@@ -14,13 +14,16 @@ struct JokeView: View {
     //0.0 is invisible, 1.0 is visible
     @State var punchLineOpacity = 0.0
     
+    //The current joke to display
+    @State var currentJoke = exampleJoke
+    
     //MARK: Computed Properties
     var body: some View {
         NavigationView{
             
             VStack{
                 
-                Text("You see, mountains aren't just funny.")
+                Text(currentJoke.setup)
                     .font(.title)
                     .multilineTextAlignment(.center)
                 
@@ -37,12 +40,13 @@ struct JokeView: View {
                     
                 })
                 
-                Text("THey are hill areas.")
+                Text(currentJoke.punchline)
                     .font(.title)
                     .multilineTextAlignment(.center)
                     .opacity(punchLineOpacity)
                 
             }
+            .padding()
             .navigationTitle("Random Jokes")
         }
     }
