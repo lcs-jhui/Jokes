a6eb0c86872fdf98d5545be7af14d1923771ecd4
Justin Hui <jhui@lcs.on.ca>
2023-04-18 09:23:49 -0400

    Added initial support for switching between views using a tab bar.

--- Jokes/JokesApp.swift
+++ Jokes/JokesApp.swift
@@ -11,9 +11,17 @@ import SwiftUI
 struct JokesApp: App {
     var body: some Scene {
         WindowGroup {
-            JokeView()
-            //Make the database avaiable to all child views through the environment
-                .environment(\.blackbirdDatabase, AppDatabase.instance)
+            TabView{
+                JokeView()
+                    .tabItem{
+                        Label("Fresh", systemImage: "carrot")
+                    }
+                
+                FavouritesView()
+                    .tabItem{
+                        Label("Favourites", systemImage: "face.smiling")
+                    }
+            }
         }
     }
 }
